5aea0f0434034f68e0ba6201bfda210409607206
unknown <dubeyanuj12345@gmail.com>
2024-05-29 00:09:04 +0530

    Relations between Tables

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username,email){
+  constructor(username,email,cart,id){
     this.name=username;
     this.email=email;
+    this.cart=cart;
+    this._id= id
   }
 
   save(){
@@ -17,6 +19,35 @@ class User {
   static findById(userId){
     const db = getDb();
     return db.collection('users').findOne({_id:new ObjectId(userId)});
+  }
+
+  addToCart(product){
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString()
+    })
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if  (cartProductIndex>=0){
+      newQuantity= this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else{
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity:newQuantity
+      });
+    }
+
+
+
+    const updatedCart = { items: updatedCartItems };
+    const db=getDb();
+    return db
+    .collection('users')
+    .updatedOne(
+      {_id: new ObjectId(this._id)},
+      {$et : { cart :updatedCart}}
+    );
   }
 }
 
